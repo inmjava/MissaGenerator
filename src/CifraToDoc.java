fbdb6cde611f69f0773237df6ece2406259a3692
Ivan <Ivan@pc-ivan>
2014-10-28 23:25:06 -0200

    Primeiro Commit

---
+++ src/CifraToDoc.java
@@ -0,0 +1,33 @@
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+
+public class CifraToDoc {
+	
+	public static void main(String[] args) throws IOException, InterruptedException {
+
+		XWPFDocument doc = new XWPFDocument();
+
+        XWPFParagraph p = doc.createParagraph();
+
+        XWPFRun run = p.createRun();
+        
+        Document doc2 = Jsoup.connect("http://www.cifraclub.com.br/celina-borges/lava-me/imprimir.html").get();
+
+        run.setText(doc2.select("pre").text());
+        run.setFontFamily("Arial");
+        run.setFontSize(10);
+
+        FileOutputStream out = new FileOutputStream("simple.docx");
+        doc.write(out);
+        out.close();
+	}
+
+}
---
+++ src/CifraToPpt.java
@@ -0,0 +1,47 @@
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.hslf.model.Slide;
+import org.apache.poi.hslf.model.TextBox;
+import org.apache.poi.hslf.usermodel.SlideShow;
+
+
+public class CifraToPpt {
+	
+	public static void main(String[] args) throws IOException, InterruptedException {
+
+		//create a new empty slide show
+	    SlideShow ppt = new SlideShow();
+
+	    //add first slide
+	    Slide s1 = ppt.createSlide();
+	    
+	    TextBox title = s1.addTitle();
+	    title.setText("Hello, World!\n" +
+	    		"Hello, World!\n" +
+	    		"Hello, World!\n" +
+	    		"Hello, World!\n" +
+	    		"Hello, World!\n" +
+	    		"Hello, World!\n" +
+	    		"Hello, World!\n" +
+	    		"Hello, World!\n" +
+	    		"Hello, World!\n" +
+	    		"Hello, World!\n" +
+	    		"Hello, World!\n" +
+	    		"");
+
+	    //add second slide
+	    Slide s2 = ppt.createSlide();
+	    
+	    title = s2.addTitle();
+	    title.setText("Hello, World!");
+	    
+	    //save changes in a file
+	    FileOutputStream out = new FileOutputStream("slideshow.ppt");
+	    ppt.write(out);
+	    out.close();
+	                 
+	}
+
+}
