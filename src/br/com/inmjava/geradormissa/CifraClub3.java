9cb5b263882fda05bf8343fef101d0a56487216c
Ivan <Ivan@pc-ivan>
2015-01-17 11:11:05 -0200

    Testes Doc

--- src/br/com/inmjava/geradormissa/CifraClub3.java
+++ src/br/com/inmjava/geradormissa/CifraClub3.java
@@ -10,8 +10,8 @@ public class CifraClub3 {
 	
 	public static void main(String[] args) throws IOException, InterruptedException {
 
-		Document doc = Jsoup.connect("http://www.cifraclub.com.br/maria-do-rosario/louvando-a-maria/imprimir.html").get();
-		System.out.println(doc);
+		Document jsoupDoc = Jsoup.connect("http://www.cifraclub.com.br/maria-do-rosario/louvando-a-maria/imprimir.html").get();
+    	System.out.println(jsoupDoc.select("h1").text());
 		
 	}
 
